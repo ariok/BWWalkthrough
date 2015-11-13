15c488db9f60ed168bbb2e96f0ec58975b6b2df8
kokoro <kokoro_hirai@mac.com>
2015-11-13 15:38:12 +0900

    add framework scheme

---
+++ BWWalkthrough/BWWalkthrough.h
@@ -0,0 +1,19 @@
+//
+//  BWWalkthrough.h
+//  BWWalkthrough
+//
+//  Created by HiraiKokoro on 2015/11/13.
+//  Copyright © 2015年 Yari D'areglia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BWWalkthrough.
+FOUNDATION_EXPORT double BWWalkthroughVersionNumber;
+
+//! Project version string for BWWalkthrough.
+FOUNDATION_EXPORT const unsigned char BWWalkthroughVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BWWalkthrough/PublicHeader.h>
+
+
