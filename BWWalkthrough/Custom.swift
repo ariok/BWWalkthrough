8e8f7eea893cabf11ddff3034c9d2a6b3688e9d3
*OC2sC7ylDj <anhdk@hblab.vn>
2021-02-07 21:36:52 +0700

    Add new file

---
+++ BWWalkthrough/Custom.swift
@@ -0,0 +1,13 @@
+//
+//  Custom.swift
+//  BWWalkthrough
+//
+//  Created by Admin on 2/7/21.
+//  Copyright © 2021 Bitwaker. All rights reserved.
+//
+
+import Foundation
+
+struct CustomClass {
+    static let customVar = 10
+}
